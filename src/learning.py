e87e2c2b7b60b30d8f9b0fe6fbfd08eca67c7971
Senne Deproost <sennedeproost@hotmail.com>
2019-11-27 21:47:32 +0100

    Added save and load functions to PolicyTree class

--- src/learning.py
+++ src/learning.py
@@ -1,8 +1,38 @@
+# Reinforcement Learning Policy Tree
 from anytree import Node, RenderTree
+from anytree.exporter import JsonExporter
+from anytree.importer import JsonImporter
+import json
 
 class PolicyTree:
     def __init__(self, agent_name):
         self.nodes = []
+        self.tree = None
         self.agent_name = agent_name
+
+    def save(self):
+        extension = ".rpt"
+        dir_name = "../data/policies/"
+        file_name = dir_name + self.agent_name + extension
+        exporter = JsonExporter(indent=2, sort_keys=True)
+        with open(file_name, 'w') as f:
+            json.dump(exporter.export(self.tree), f)
+            f.close()
+
+    def load(self):
+        extension = ".rpt"
+        dir_name = "../data/policies/"
+        file_name = dir_name + self.agent_name + extension
+        importer = JsonImporter()
+        data = None
+        with open(file_name, "r") as f:
+            data = json.loads(f)
+            f.close()
+        self.tree = importer.import_(data)
+
+
+
+
+
 
 
