67309fda391e26e6c0f386a71ffe0b9100b31bda
Senne Deproost <sennedeproost@hotmail.com>
2019-12-04 17:30:02 +0100

    Added PolicyMatrix

--- src/PlicyMatrix.py
+++ src/PlicyMatrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class TransMatrix:
     def __init__(self):
@@ -11,3 +12,13 @@ class PolicyMatrix:
     def __init__(self, n_states, n_actions):
         self.n_action = n_actions
         self.n_states = n_states
+        self.policy = np.zeroes([n_states, n_actions])
+
+    def best(self, state_indx):
+        return np.argmax(self.policy[state_indx])
+
+    def random(self):
+        return random.randint(0, self.n_actions)
+
+    def update(self, old_state, action, new_state, reward):
+        self.policy[old_state][action] += 0.9*(reward + 0.9*np.max(self.policy[new_state, :]) - self.policy[old_state, action])
