88c6353667a45294641c55bca8cda411623e7334
Senne Deproost <sennedeproost@hotmail.com>
2019-12-13 13:12:45 +0100

    Fixed speech recognition bug

--- src/devices/Microphone.py
+++ src/devices/Microphone.py
@@ -7,22 +7,15 @@ from debug import *
 class Microphone:
     def __init__(self, owner):
         self.owner = owner
-        try:
-            self.recorder = sr.Recognizer()
-            self.mic = sr.Microphone()
-        except:
-            pass
+        self.recorder = sr.Recognizer()
+        self.mic = sr.Microphone()
 
     def observe(self):
-        input = ""
-        try:
-            with self.mic as source:
-                log("Listening...", self.owner)
-                audio = self.recorder.listen(source)
-                input = self.recorder.recognize_google(audio)
-                log("Heard: " + input, self.owner)
-        except:
-            pass
+        with self.mic as source:
+            log("Listening...", self.owner)
+            audio = self.recorder.listen()
+            input = self.recorder.recognize_google(audio)
+            log("Heard: " + input, self.owner)
         return input
 
 
